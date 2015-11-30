eddc8aac2241964ab9048ca1a6bf2da6a3ac05be
Alexandra-Bondar <s_y_a@tut.by>
2015-11-30 19:37:04 +0300

    project

---
+++ ChatApp/src/sample/Main.java
@@ -0,0 +1,87 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+//import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    // peremennaya zadauschaya server or client
+    private boolean isServer = true;
+
+    //connect to server from client
+    private TextArea messages = new TextArea();
+    private NetworkConnection connection = isServer ? createServer():createClient();
+
+    //create a connection
+    private Parent createContent(){
+        // pole vvoda sms i ego rabota
+        messages.setPrefHeight(450);
+        TextField input = new TextField();
+        input.setOnAction(event -> {
+            String message = isServer ? "User1: " : "User2: ";
+           //message = color("Red");
+            message += input.getText();
+            input.clear();
+
+            messages.appendText(message + "\n");
+
+            try{
+                connection.send(message);
+            }
+            catch(Exception e) {
+                messages.appendText("Failed to send\n");
+            }
+        });
+
+        VBox root = new VBox(20, messages, input); //!!!!!!!!!!!! pomestit' v eto pole smile's
+        root.setPrefSize(300, 500);
+        return root;
+
+    }
+
+    @Override
+    public void init() throws Exception{
+    connection.startConnection();
+}
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("ChatClick");
+        primaryStage.setScene(new Scene(createContent()));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws  Exception{
+            connection.closeConnection();
+    }
+
+    private Server createServer() {
+        return new Server(55555, data -> {
+            Platform.runLater(() -> {
+                messages.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
+
+    private Client createClient() {
+        return new Client("127.0.0.1", 55555, data -> {
+            Platform.runLater(() -> {
+                messages.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
